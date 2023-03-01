9d98445ab35f24d03ca9427e83bc7edaba7a2723
deutschkihun@gmail.com <deutschkihun@gmail.com>
2023-03-01 14:46:04 +0900

    handle mobile screen

--- src/components/TodoGeneratror.js
+++ src/components/TodoGeneratror.js
@@ -1,9 +1,11 @@
-import React, { useRef } from "react";
-import { TextInput, Button } from "@mantine/core";
+import React, { useRef, useState } from "react";
+import { TextInput, Modal, Button, Group } from "@mantine/core";
 import { IconFilePlus } from "@tabler/icons-react";
 import { IconTextPlus } from "@tabler/icons-react";
+import { IconWindmill } from "@tabler/icons-react";
 
 const TodoGeneratror = ({ todoList }) => {
+  const [opened, setOpened] = useState(false);
   const inputRef = useRef();
   const onClickHandler = () => {
     if (inputRef.current.value === "" || !inputRef.current) {
@@ -21,20 +23,56 @@ const TodoGeneratror = ({ todoList }) => {
   };
 
   return (
-    <div className="todo-generator">
-      <TextInput
-        icon={<IconTextPlus size={16} />}
-        ref={inputRef}
-        placeholder="Insert new todo title"
-        type="text"
-        mr={10}
-        mb={10}
-        style={{ flex: 1 }}
-      />
-      <Button leftIcon={<IconFilePlus size={18} />} onClick={onClickHandler}>
-        create new todo
-      </Button>
-    </div>
+    <>
+      <div className="todo-generator">
+        <TextInput
+          icon={<IconTextPlus size={16} />}
+          ref={inputRef}
+          placeholder="Insert content"
+          type="text"
+          mr={10}
+          mb={10}
+          style={{ flex: 1 }}
+        />
+        <Button leftIcon={<IconFilePlus size={18} />} onClick={onClickHandler}>
+          Create new todo
+        </Button>
+      </div>
+      <div className="todo-generator-mobile">
+        <Modal
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title="Create new todo"
+        >
+          <TextInput
+            icon={<IconTextPlus size={16} />}
+            ref={inputRef}
+            placeholder="Insert content"
+            type="text"
+            fullWidth
+            mb={10}
+          />
+          <Button
+            leftIcon={<IconFilePlus size={18} />}
+            onClick={onClickHandler}
+            fullWidth
+          >
+            create
+          </Button>
+        </Modal>
+
+        <Group position="center">
+          <Button
+            onClick={() => setOpened(true)}
+            mb={10}
+            fullWidth
+            leftIcon={<IconWindmill size={16} />}
+          >
+            Create todo
+          </Button>
+        </Group>
+      </div>
+    </>
   );
 };
 
--- src/components/TodoSearchFilter.js
+++ src/components/TodoSearchFilter.js
@@ -1,35 +1,89 @@
-import React from "react";
-import { TextInput, SegmentedControl } from "@mantine/core";
+import React, { useState } from "react";
+import {
+  TextInput,
+  SegmentedControl,
+  Modal,
+  Button,
+  Group,
+} from "@mantine/core";
+
 import { IconSearch } from "@tabler/icons-react";
+import { IconDoorEnter } from "@tabler/icons-react";
 
 const TodoSearchFilter = ({ setSearch, filter, setFilter }) => {
-  return (
-    <div className="todo-search-filter">
-      <TextInput
-        placeholder="Search"
-        icon={<IconSearch size={16} />}
-        type="text"
-        onChange={(e) => setSearch(e.target.value)}
-        style={{ flex: 1 }}
-        mr={10}
-        mb={10}
-      />
+  const [opened, setOpened] = useState(false);
 
-      <SegmentedControl
-        value={filter}
-        onChange={setFilter}
-        color="blue"
-        transitionDuration={500}
-        transitionTimingFunction="linear"
-        radius="md"
-        size="m"
-        data={[
-          { label: "Complete", value: "complete" },
-          { label: "In progress", value: "in progress" },
-          { label: "All", value: "all" },
-        ]}
-      />
-    </div>
+  return (
+    <>
+      <div className="todo-search-filter">
+        <TextInput
+          placeholder="Search"
+          icon={<IconSearch size={16} />}
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          style={{ flex: 1 }}
+          mr={10}
+        />
+
+        <SegmentedControl
+          value={filter}
+          onChange={setFilter}
+          color="blue"
+          transitionDuration={500}
+          transitionTimingFunction="linear"
+          radius="md"
+          size="m"
+          data={[
+            { label: "Complete", value: "complete" },
+            { label: "In progress", value: "in progress" },
+            { label: "All", value: "all" },
+          ]}
+        />
+      </div>
+      <div className="todo-search-filter-mobile">
+        <Modal
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title="Filter option"
+        >
+          <TextInput
+            placeholder="Search"
+            icon={<IconSearch size={16} />}
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            style={{ flex: 1 }}
+          />
+          <SegmentedControl
+            value={filter}
+            onChange={setFilter}
+            color="blue"
+            transitionDuration={500}
+            transitionTimingFunction="linear"
+            radius="md"
+            size="m"
+            data={[
+              { label: "Complete", value: "complete" },
+              { label: "In progress", value: "in progress" },
+              { label: "All", value: "all" },
+            ]}
+            style={{ flex: 1 }}
+            mt={10}
+            fullWidth
+          />
+        </Modal>
+
+        <Group position="center">
+          <Button
+            onClick={() => setOpened(true)}
+            mb={10}
+            style={{ flex: 1 }}
+            leftIcon={<IconDoorEnter size={16} />}
+          >
+            Open filter option
+          </Button>
+        </Group>
+      </div>
+    </>
   );
 };
 
